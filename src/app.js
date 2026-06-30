2d989aa55fe31344ec23c8a762e0b5378cff75ef
Ritesh Jha <ritesh.kr.jha@myprolist.com>
2026-06-30 18:56:38 +0530

    Tested routing

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const app = express();
 
+
+
+app.use("/test/12",( req, res)=>{
+    res.send("Heluu from test")
+})
+
+app.use("/xyz",( req, res)=>{
+    res.send("Heluu from xyz")
+})
+
 app.use("/",(req, res)=>{
    res.send("heluu from node.js")
 })
-
 
 app.listen(7777, ()=>{
     console.log('Server is running fine on 7777')
