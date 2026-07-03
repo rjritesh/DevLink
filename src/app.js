d841d0f3a14d4369792b54cfeb71223722e0528b
Ritesh jha <riteshjha24060@gmail.com>
2026-07-03 16:35:48 +0530

    Dynamic data post from api

--- src/app.js
+++ src/app.js
@@ -5,8 +5,13 @@ const User = require("./models/user");
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = express();
+app.use(express.json())
+
+
 
 connectDB()
+
+
   .then(() => {
     console.log("Database connected successfully");
 
@@ -18,15 +23,13 @@ connectDB()
     console.error("Database connection failed:", err);
   });
 
-app.post("/signup", async (req, res) => {
+
+
+  app.post("/signup", async (req, res) => {
+  
+  
   try {
-    const user = new User({
-      firstName: "Ritesh",
-      lastName: "Jha",
-      age: 22,
-      gender: "M",
-      emailId: "ritesh@gmail.com",
-    });
+    const user = new User(req.body);
 
     await user.save();
 
