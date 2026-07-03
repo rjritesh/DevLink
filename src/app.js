f9d2801a8a91cd30b24a2dbd2f3073003447e94d
Ritesh jha <riteshjha24060@gmail.com>
2026-07-03 09:42:16 +0530

    Tested signup route with hardcode data

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const dns = require("dns");
+const User = require("./models/user");
 
-dns.setServers([
-    '1.1.1.1',
-    '8.8.8.8',
-])
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = express();
 
 connectDB()
@@ -20,18 +18,21 @@ connectDB()
     console.error("Database connection failed:", err);
   });
 
+app.post("/signup", async (req, res) => {
+  try {
+    const user = new User({
+      firstName: "Ritesh",
+      lastName: "Jha",
+      age: 22,
+      gender: "M",
+      emailId: "ritesh@gmail.com",
+    });
 
+    await user.save();
 
-
-  app.post("/signup", (req, res)=>{
-const user = new User ({
-    firstName: "Ritesh",
-    lastName: "jha",
-    age: 22,
-    gender: "M",
-    emailId: "ritesh@gmail.com",
-
-})
-    
-    res.send("User created successfully")
-  })
+    res.send("User added to db successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(400).send(error.message);
+  }
+});
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = async ()=>{
-   await mongoose.connect("mongodb+srv://riteshDev:2Gi92nbmkNkMc3As@namastenode.xwcial4.mongodb.net/")
+   await mongoose.connect("mongodb+srv://riteshDev:2Gi92nbmkNkMc3As@namastenode.xwcial4.mongodb.net/DevLink")
 };
 
 
