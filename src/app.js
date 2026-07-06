e32b8459c398fc49548ffd8e5d907b3481299a0c
Ritesh jha <riteshjha24060@gmail.com>
2026-07-06 14:41:27 +0530

    Fetched single user from API

--- src/app.js
+++ src/app.js
@@ -5,13 +5,9 @@ const User = require("./models/user");
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = express();
-app.use(express.json())
-
-
+app.use(express.json());
 
 connectDB()
-
-
   .then(() => {
     console.log("Database connected successfully");
 
@@ -23,11 +19,7 @@ connectDB()
     console.error("Database connection failed:", err);
   });
 
-
-
-  app.post("/signup", async (req, res) => {
-  
-  
+app.post("/signup", async (req, res) => {
   try {
     const user = new User(req.body);
 
@@ -37,5 +29,16 @@ connectDB()
   } catch (error) {
     console.error(error);
     res.status(400).send(error.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  
+    const userEmail = req.body.emailId;
+  try {
+   const user =  await User.find({ emailId :userEmail});
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Something went wrong..");
   }
 });
