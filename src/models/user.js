61176a8e44c9777c18a249cadf60a4ac3ce3d5d9
Ritesh jha <riteshjha24060@gmail.com>
2026-07-07 16:29:34 +0530

    Added schema type and validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,52 @@
-const mongoose = require("mongoose")
-
-
-const userschema = new mongoose.Schema({
-    firstName:{
-        type: String,
+const mongoose = require("mongoose");
+const validator = require("validator");
+const userschema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 4,
+      maxLength: 50,
     },
-       lastName:{
-        type: String,
-    },   age:{
-        type: Number,
-    },   emailId:{
-        type: String,
-    },   password:{
-        type: String,
-    },   gender:{
-        type: String,
-    }
-})
+    lastName: {
+      type: String,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 100,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value){
+        if(!validator.isEmail(value)){
+            throw new error("Invalid email")
+        }
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength:8,
+    },
+    gender: {
+      type: String,
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user",
+    },
+  },
+  { timestamps: true },
+);
 
-module.exports = mongoose.model("User", userschema); 
+module.exports = mongoose.model("User", userschema);
