5df5469573e55e14ed778fb394cfd7650ce00d96
Ritesh jha <riteshjha24060@gmail.com>
2026-07-13 15:35:30 +0530

    Password encryption using bcrypt

--- src/app.js
+++ src/app.js
@@ -2,16 +2,15 @@ const express = require("express");
 const connectDB = require("./config/database");
 const dns = require("dns");
 const User = require("./models/user");
-const { queryObjects } = require("v8");
-const { error } = require("console");
+
 const { validateSignupData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = express();
 app.use(express.json());
 
-
-///database connection 
+///database connection
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
@@ -24,16 +23,28 @@ connectDB()
     console.error("Database connection failed:", err);
   });
 
-
-  ////post api
+////post api
 
 app.post("/signup", async (req, res) => {
-
-
-
   try {
-      validateSignupData(req)
-    const user = new User(req.body);
+    const { firstName, lastName, password, emailId } = req.body;
+
+    //validation of data
+    validateSignupData(req);
+
+    //Encrytpion of the password
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    console.log(hashPassword);
+
+    //Creating a new instace of the User modal
+
+    const user = new User({
+      firstName, 
+      lastName,
+      emailId,
+      password : hashPassword,
+    });
 
     await user.save();
 
@@ -44,68 +55,55 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-
 //get the single user from API
 app.get("/user", async (req, res) => {
-  
   const userEmail = req.body.emailId;
   try {
-   const user =  await User.find({ emailId :userEmail});
+    const user = await User.find({ emailId: userEmail });
     res.send(user);
   } catch (error) {
     res.status(400).send("Something went wrong..");
   }
 });
 
-
-
 //get all the feeds from api
-app.get("/feed", async (req, res)=>{
-
-  try{
+app.get("/feed", async (req, res) => {
+  try {
     const user = await User.find({});
-    res.send(user)
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
   }
-  catch(error){
-    res.status(400).send("Something went wrong")
-  }
-})
-
+});
 
 //delete api/
-app.delete("/user", async(req, res)=>{
+app.delete("/user", async (req, res) => {
   const userId = req.body.userId;
-
-  try{
-const user = await User.findByIdAndDelete(userId);
-res.send("user deleted successfully")
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully");
+  } catch (error) {
+    res.status(400).send("Someting wrong");
   }
-  catch(error){
-res.status(400).send("Someting wrong")
-  }
-})
+});
 
-
-
-////update api    
+////update api
 
 app.patch("/user/:id", async (req, res) => {
   const userId = req.params.id;
-  const data = req.body;   // Agar Postman me data object bhej rahe ho
-
+  const data = req.body; // Agar Postman me data object bhej rahe ho
   try {
-    const allowedUpdates = ["photoUrl", "gender",  "about", "skills"];
-
+    const allowedUpdates = ["photoUrl", "gender", "about", "skills"];
     const isUpdateAllowed = Object.keys(data).every((k) =>
-      allowedUpdates.includes(k)
+      allowedUpdates.includes(k),
     );
 
     if (!isUpdateAllowed) {
       throw new Error("Update not allowed!!");
     }
 
-    if(data.skills && data?.skills.length >  10 ){
-      throw new Error("Skills can not be more than 10")
+    if (data.skills && data?.skills.length > 10) {
+      throw new Error("Skills can not be more than 10");
     }
 
     await User.findByIdAndUpdate(userId, data, {
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,22 +1,18 @@
-const validator = require("validator")
+const validator = require("validator");
 
+const validateSignupData = (req) => {
+  const { firstName, lastName, password, emailId } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("Name is invalid");
+  } else if (firstName.length < 4) {
+    throw new Error("Name is invalid");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is invalid");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error(" Please Enter a strong password ");
+  }
+};
 
-const validateSignupData = (req)=>{
-const { firstName, lastName, password, emailId} = req.body;
-if(!firstName || !lastName){
-    throw new Error ( "Name is invalid");
-}
-else if(firstName.length < 4 ){
-    throw new Error ( "Name is invalid")
-}
-else if (!validator.isEmail(emailId)){
-throw new Error ( "Email is invalid")
-}
-else if ( !validator.isStrongPassword(password)){
-    throw new Error( " Please Enter a strong password ")
-}
-}   
-
-module.exports= {
-    validateSignupData
-}
+module.exports = {
+  validateSignupData,
+};
